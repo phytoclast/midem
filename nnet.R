096d1a2a7e0a928bb2ed03c2b04c5c155a9d7b96
Schmidt <12001001973813@FEDIDCARD.GOV>
2023-01-09 17:14:46 -0500

    Update nnet.R

--- nnet.R
+++ nnet.R
@@ -5,13 +5,44 @@ library(ranger)
 
 brk.pts <- readRDS('output/brk.pts.RDS')
 hmnf.chm2 <- readRDS('output/hmnf.chm3.RDS')
+rpmod <- function(age){
+  b0= 2.0434
+  b1= 0.9978
+  b2= -0.0147
+  b3= 1.0937
+  b4= -0.0035
+  S= 58#subset(redpine,SITE_INDEX_SPP %in%  'PIRE') $ SITE_INDEX %>% median()
+  
+  (0+b0*(S^b1)*(1-exp(b2*age))^(b3*S^b4)   )*0.3048}
+
+asmod <- function(age){
+  b0= 5.2188
+  b1= 0.6855
+  b2= -0.0301
+  b3= 50.0071
+  b4= -0.8695
+  S= 63#subset(aspen,SITE_INDEX_SPP %in%  c('POGR4','POTR5')) $ SITE_INDEX %>% median()
+  (0+b0*(S^b1)*(1-exp(b2*age))^(b3*S^b4)   )*0.3048}
+
+brk.pts <- brk.pts %>% mutate(age=50, pred1 = asmod(age = 50), pred2 =  rpmod(age = 50), jackpine = 0, redpine = 0, whitepine = 0, aspen = 0, maple = 1, oak = 0, status = 'raster')
+
+hmnf.chm2$status <- 'training'
+
+mergedset <- dplyr::bind_rows(brk.pts, hmnf.chm2)
+
+
 inputcols <-   c("chm.max","solar","hilly","age","pred1","pred2","toip","toin","bt","tgs","ppt","p1","p2","p3","p4","T150_AWC","T50_sand","T150_sand","T50_clay","T150_clay","T50_OM","T150_OM","T50_pH","Water_Table","wet","spodic","carbdepth","Bhs","jackpine","redpine","whitepine","aspen","oak","maple")
 
 # inputcols2 <- c(solar,hilly,age,pred1,pred2,toip,toin,bt,tgs,ppt,p1,p2,p3,p4,T150_AWC,T50_sand,T150_sand,T50_clay,T150_clay,T50_OM,T150_OM,T50_pH,Water_Table,wet,spodic,carbdepth,Bhs,jackpine,redpine,whitepine,aspen,oak,maple)
 
-hmnf.chm2.norm <- hmnf.chm2 %>% mutate(across(inputcols, ~ scale(.)[, 1]))
+mergedset.norm <- mergedset %>% mutate(across(inputcols, ~ scale(.)[, 1]))
+# mergedset.norm2 <- mergedset %>% select(all_of(inputcols)) %>% scale(.)[, 1]
+meanchm <-  mean(hmnf.chm2$chm.max)
+sdchm <-  sd(hmnf.chm2$chm.max)
 
-trainset = hmnf.chm2.norm[sample(rownames(hmnf.chm2.norm),10000),]
+trainset <- subset(mergedset.norm, status  %in% 'training')
+trainset <- trainset %>% mutate(prob = ifelse(hilly > 0, 4,1)) 
+trainset = trainset[sample(rownames(trainset),20000, prob=trainset$prob),]
 testingrows <- sample(rownames(trainset), floor(nrow(trainset)*0.1))
 train <- trainset[!rownames(trainset) %in% testingrows,]
 test <- trainset[rownames(trainset) %in% testingrows,]
@@ -60,7 +91,7 @@ rf <- ranger(formular
 
 
 nn <- neuralnet(formular
-               , data=train,  hidden=c(2,2), threshold = 0.2, act.fct	= 'tanh')
+               , data=train,  hidden=c(2,2), threshold = 0.2, stepmax = 2e+05, act.fct	= 'tanh')
 
 plot(nn)
 nn$result.matrix
@@ -118,4 +149,18 @@ mean(resids$res4)^0.5
 mean(resids$resn)^0.5
 
 
+mergedset.pred <- mergedset.norm %>% mutate(resn = (predict(nn, newdata=mergedset.norm))) %>% subset(status %in% 'raster') 
+
+
+mergedset.pred <- mergedset.pred %>% mutate(newchm = resn*sdchm + meanchm)
+
+library(terra)
+dem <- rast('D:/GIS/DEM/hmnfdem30.tif'); names(dem) = 'dem'
+
+nnmap <- rast(cbind(x=mergedset.pred$x, y=mergedset.pred$y, resn=mergedset.pred$newchm), type="xyz", crs=crs(dem))
+
+plot(nnmap)
+writeRaster(nnmap, 'output/nnmaple.tif', overwrite=T)
 #regression-enhanced random forest and ensemble models incorporating linear models?
+#
+
